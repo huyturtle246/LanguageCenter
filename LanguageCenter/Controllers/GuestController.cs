0bfde27d56c28f957609a14bd69468299f127b34
HongNhat <hongnhatnt08022005@gmail.com>
2026-06-02 14:26:24 +0700

    Mua cho tui figure

---
+++ LanguageCenter/Controllers/GuestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageCenter.Controllers
+{
+    public class GuestController : Controller
+    {
+        // GET: Guest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Mua cho tui figure
+    }
+}
