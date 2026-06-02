0f9bce795958420e7129c332e003f2d47d95f11d
HongNhat <hongnhatnt08022005@gmail.com>
2026-06-02 13:54:26 +0700

    add teacher controller

---
+++ LanguageCenter/Controllers/TeacherController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageCenter.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
